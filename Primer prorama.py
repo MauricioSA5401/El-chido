bfad03b9aa00df114c5ef04329f34cf281fb43ea
MauricioSA5401 <134406537+MauricioSA5401@users.noreply.github.com>
2024-10-21 10:41:09 -0600

    Add files via upload

---
+++ Primer prorama.py
@@ -0,0 +1,10 @@
+
+print("Esto es una suma")
+num1 = 3
+num2 = 5
+resultado = num1 + num2 
+print(resultado) 
+print("siuuuuuu")
+ 
+
+ 
---
+++ SegundoPrograma.py
@@ -0,0 +1,25 @@
+mensaje = "hola"
+mensaje += " Mau"
+siu = " como"
+siuu = " estas"
+lol = "?"
+espacio = " "
+num1 = 3 
+num1 = str(num1)
+print(mensaje + siu + siuu + lol + num1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
